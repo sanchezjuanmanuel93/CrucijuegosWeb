c21e8510826c265a7b1c88b427a72d14817a0c34
sanchezjuanmanuel93 <sanchez.juanmy@gmail.com>
2015-07-21 09:41:21 -0300

    web cruci

---
+++ CrucijuegosWeb/enviomail.php
@@ -0,0 +1,11 @@
+<?php
+$to = "sanchez.juanmy@gmail.com";
+$subject = $_POST['subject'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$text = $_POST['text'];
+$message = "<html><body><h3>$name</h3></br><b>$email</><p>$text</p></body></html>";
+$headers = "Content-type: text/html; charset=iso-8859-1\r\n";
+mail($to, $subject, $message, $headers);
+echo $name;
+?>
---
+++ CrucijuegosWeb/web.sh
@@ -0,0 +1,10 @@
+wget \
+     --recursive \
+     --no-clobber \
+     --page-requisites \
+     --html-extension \
+     --convert-links \
+     --restrict-file-names=windows \
+     --domains demo.fifothemes.com \
+     --no-parent \
+         http://demo.fifothemes.com/agency/
