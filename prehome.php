bd48fcde2b8668e937cabc4f613a6a49f38b8ce3
root <root@juan>
2015-10-14 09:52:28 -0300

    prehome responsive alineado

--- prehome.php
+++ prehome.php
@@ -1,46 +1,42 @@
 <!doctype html>
 <html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
-
-	<link rel="stylesheet" href="http://www.crucijuegos.com/sitefiles/css/reset.css"> <!-- Resource style -->
-	<link rel="stylesheet" href="http://www.crucijuegos.com/sitefiles/css/style.css"> <!-- Resource style -->
-
-	<title>CRUCIJUEGOS</title>
-</head>
-<body>
-	<div class="projects-container">
-		<ul>
-        	
-			<li class="cd-single-project image-1">
-            	<a href="http://www.crucijuegos.com/salas/esp/-crucijuegos.php">
-                    <div class="cd-title">
-                    <img src="http://www.crucijuegos.com/productos/sitefiles/img/assets/logoNew.png"  class="img-responsive"/>
-                        <h2>Salas de entretenimiento</h2>
-                        <p>Visite nuestras salas de juegos: Games, Pistas de patinaje, Eventos, Cumpleaños</p>
-                        <span class="btn">Ver las Salas</span>
-                    </div> <!-- .cd-title -->
-				 </a>
-             </li>
-           
-			
-			<li class="cd-single-project image-2">
-            	<a href="http://www.crucijuegos.com/productos/esp/-crucijuegos.php">
-                    <div class="cd-title">
-                    <img src="http://www.crucijuegos.com/productos/sitefiles/img/assets/logo_crucijuegos.png"  class="img-responsive"/>
-                        <h2>Juegos de espacio público</h2>
-                        <p>Conozca nuestros productos para:
-						Municipios, hoteles, Restaurantes, Salones de fiestas y Particulares</p>
-						<span class="btn">Ver los productos</span>
-                    </div> <!-- .cd-title -->
-			 	</a>
-           	</li>
-          
-		</ul>
-	</div> <!-- .project-container -->
-
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>        
+        <link rel="stylesheet" href="http://www.crucijuegos.com/sitefiles/css/reset.css"> <!-- Resource style -->
+        <link rel="stylesheet" href="style.css"> <!-- Resource style -->
+        <!--
+        <link rel="stylesheet" href="http://www.crucijuegos.com/sitefiles/css/style.css">  Resource style 
+        -->
+        <title>CRUCIJUEGOS</title>
+    </head>
+    <body>
+        <div class="projects-container">
+            <ul>        	
+                <li class="cd-single-project image-1">
+                    <a href="http://www.crucijuegos.com/salas/esp/-crucijuegos.php">
+                        <div class="cd-title">
+                            <img src="http://www.crucijuegos.com/productos/sitefiles/img/assets/logoNew.png"  class="img-responsive"/>
+                            <h2>Salas de entretenimiento</h2>
+                            <p>Visite nuestras salas de juegos: Games, Pistas de patinaje, Eventos, Cumpleaños</p>
+                            <span class="btn">Ver las Salas</span>
+                        </div> <!-- .cd-title -->
+                    </a>
+                </li>
+                <li class="cd-single-project image-2">
+                    <a href="http://www.crucijuegos.com/productos/esp/-crucijuegos.php">
+                        <div class="cd-title">
+                            <img src="http://www.crucijuegos.com/productos/sitefiles/img/assets/logo_crucijuegos.png"  class="img-responsive"/>
+                            <h2>Juegos de espacio público</h2>
+                            <p>Conozca nuestros productos para:
+                                Municipios, hoteles, Restaurantes, Salones de fiestas y Particulares</p>
+                            <span class="btn">Ver los productos</span>
+                        </div> <!-- .cd-title -->
+                    </a>
+                </li>
+            </ul>
+        </div> <!-- .project-container -->
+    </body>
 </html>
